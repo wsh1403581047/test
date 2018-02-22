8d21925a12b0f807a0ced6434100deda95de169d
JustNickNameWSH <1403581047@qq.com>
2018-02-22 22:49:38 +0800

    反射与注解

--- springboot/src/main/java/com/example/springboot/springboot/annotation/ReflectAnnotationDemo.java
+++ springboot/src/main/java/com/example/springboot/springboot/annotation/ReflectAnnotationDemo.java
@@ -2,7 +2,8 @@ package com.example.springboot.springboot.annotation;
 
 
 import java.lang.annotation.*;
-
+import java.lang.reflect.Method;
+//注解和反射
 public class ReflectAnnotationDemo {
     public static void main(String[] args) throws Exception {
        Class<?> classType = AnnotationTest2.class;
@@ -10,7 +11,23 @@ public class ReflectAnnotationDemo {
         if(b){
             Description des = classType.getAnnotation(Description.class);
             System.out.println("获取到了：" + des.value());
-        }else System.out.println("没有获取到");
+
+            //反射获取类的所有方法
+            Method[] methods = classType.getDeclaredMethods();
+
+            for(Method method: methods){
+                boolean flag = method.isAnnotationPresent(Author.class);
+                if (flag){
+                    Author au = method.getAnnotation(Author.class);
+                    String auName = au.name();
+                    String auGroup = au.group();
+                    System.out.println(auName);
+                    System.out.println(auGroup);
+                }
+            }
+        }else {System.out.println("没有获取到");}
+
+
     }
 }
 @Retention(RetentionPolicy.RUNTIME)
@@ -34,5 +51,9 @@ class AnnotationTest2{
     @Author(name = "wushihao",group = "com.wsh")
     public void test(){
         System.out.println("123");
+    }
+
+    public void test2(){
+        System.out.println("567");
     }
 }
